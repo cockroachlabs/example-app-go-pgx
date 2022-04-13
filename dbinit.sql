4acd59b822001d633f47c01617d2c199f6238242
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2022-04-13 14:07:18 -0400

    Updated app to use DATABASE_URL for connection; simplified dbinit.sql

--- dbinit.sql
+++ dbinit.sql
@@ -1,11 +1,3 @@
-SET sql_safe_updates = FALSE;
-
-USE defaultdb;
-DROP DATABASE IF EXISTS bank CASCADE;
-CREATE DATABASE IF NOT EXISTS bank;
-
-USE bank;
-
 CREATE TABLE accounts (
     id UUID PRIMARY KEY,
     balance INT8
